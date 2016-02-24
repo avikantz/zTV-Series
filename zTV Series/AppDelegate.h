6f766f781808bfd4c95316b54529fec059bc2f53
avikantz <avikantsainidbz@gmail.com>
2016-02-24 21:09:02 +0530

    Initial Commit

---
+++ zTV Series/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zTV Series
+//
+//  Created by Avikant Saini on 2/24/16.
+//  Copyright © 2016 avikantz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
